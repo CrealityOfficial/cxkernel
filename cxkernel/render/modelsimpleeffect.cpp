1231846896d24f744ef4a4c45d0a42e1d07854d7
lihaowen <lihaowen@cxsw3d.com>
2023-08-09 11:54:41 +0800

    featrue[ModelSimpleEffect]:ModelSimpleEffect改用phong着色器。
    Change-Id: If717bf9f179d77afe7aacee8d8dbc9936c414ef4

--- cxkernel/render/modelsimpleeffect.cpp
+++ cxkernel/render/modelsimpleeffect.cpp
@@ -6,13 +6,14 @@ namespace cxkernel
 	ModelSimpleEffect::ModelSimpleEffect(Qt3DCore::QNode* parent)
 		: XEffect(parent)
 	{
-		addRenderPass(new qtuser_3d::XRenderPass("modelsimple"));
+		addRenderPass(new qtuser_3d::XRenderPass("phong"));
 		addPassFilter(0, "modelcapture");
 
-		setParameter("ambient", QVector4D(0.8f, 0.8f, 0.8f, 1.0f));
-		setParameter("diffuse", QVector4D(0.5f, 0.5f, 0.5f, 1.0f));
-		setParameter("specular", QVector4D(0.8f, 0.8f, 0.8f, 1.0f));
-		setParameter("stateColors", QVector4D(0.55f, 0.55f, 0.55f, 1.0f));
+		setParameter("color", QVariant::fromValue(QVector4D(0.55f, 0.55f, 0.55f, 1.0f)));
+		setParameter("ambient", QVariant::fromValue(QVector4D(0.8f, 0.8f, 0.8f, 1.0f)));
+		setParameter("diffuse", QVariant::fromValue(QVector4D(0.5f, 0.5f, 0.5f, 1.0f)));
+		setParameter("specular", QVariant::fromValue(QVector4D(0.8f, 0.8f, 0.8f, 1.0f)));
+		setParameter("specularPower", QVariant::fromValue(12.0));
 
 	}
 
